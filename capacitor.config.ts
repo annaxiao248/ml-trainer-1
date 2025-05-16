e4af0b3556cb3ec34a70a09a5e26c6c58bf7b772
annaxiao248 <100732427+annaxiao248@users.noreply.github.com>
2025-05-16 16:04:42 -0400

    capacitor

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.microbitml.app',
+  appName: 'Microbit ML',
+  webDir: 'dist'
+};
+
+export default config;
