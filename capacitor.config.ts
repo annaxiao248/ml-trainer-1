803ee7854c1bdff4eafd8f37a38ae62e64a6d195
annaxiao248 <100732427+annaxiao248@users.noreply.github.com>
2025-11-08 21:08:17 -0600

    building on ipad

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.tiilt.mltrainer',
+  appName: 'microbit-createai',
+  webDir: 'dist'
+};
+
+export default config;
